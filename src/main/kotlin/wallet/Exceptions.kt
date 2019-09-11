5be523eead1c13f81a3d9d1a7e1a9480eadee76b
sgobotta <santiago@camba.coop>
2019-09-11 01:08:04 -0300

    Exceptions: adds a BlockedAccountException

--- src/main/kotlin/wallet/Exceptions.kt
+++ src/main/kotlin/wallet/Exceptions.kt
@@ -2,3 +2,4 @@ package wallet
 
 class NoMoneyException(message: String?) : Throwable(message)
 class LoginException(message: String?) : Throwable(message)
+class BlockedAccountException(message: String?) : Throwable(message)
